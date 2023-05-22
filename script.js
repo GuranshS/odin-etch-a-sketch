e4e1b94a12f71da2d99f89181db99d653e10ff82
GuranshS <guranshsingh273@gmail.com>
2023-05-22 18:12:29 +0530

    completed functunatlity

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
+let penColor = "black";
+let penDown = true;
+
 function createBoard(size){
     let board = document.querySelector('.board');
-    let square = document.querySelectorAll("div");
-    
+    let square = board.querySelectorAll("div");
+    square.forEach((div)=> div.remove());
     board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
     board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
     
     let amount = size*size;
     for(let i=0; i<amount; i++){
         let square = document.createElement('div');
-        square.style.backgroundColor = "blue";
+        square.addEventListener("mouseover", colorSquare);
+        square.style.backgroundColor = "white";
         board.insertAdjacentElement('beforeend', square);
     }
 }
+
+createBoard(16);
 
 function changeSize(input){
     if(input>=2 || input<=100){
@@ -21,3 +27,38 @@ function changeSize(input){
         console.log("Please input a valid number");
     }
 }
+
+function colorSquare(){
+if(penDown){
+    if(penColor==="random"){
+        color1 = `hsl(${Math.random()*360}, 100%, 50%)`;
+        this.style.backgroundColor = color1;
+        document.querySelector(".randomButton").style.backgroundColor = color1;
+    }
+    else {
+        this.style.backgroundColor = penColor;
+    }
+}
+}
+
+function changePenColor(color){
+    penColor = color;
+}
+
+function resetBoard(){
+    let board = document.querySelector('.board');
+    let square = board.querySelectorAll("div");
+    square.forEach((div)=> div.style.backgroundColor = "white");
+}
+
+document.querySelector("body").addEventListener("click", (e)=>{
+    if(e.target.tagName != "BUTTON"){
+        penDown = !penDown;
+        if(penDown){
+            document.querySelector(".mode").textContent = "Pen is down";
+        }
+        else{
+            document.querySelector(".mode").textContent = "Pen is up";
+        }
+    }
+})
