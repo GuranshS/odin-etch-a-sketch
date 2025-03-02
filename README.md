5fcc18d2c4b3faf15737d4e855e5b3c7a39f7469
GuranshS <guranshsingh273@gmail.com>
2025-03-03 02:38:14 +1100

    Updated looks

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # odin-etch-a-sketch
 A pixel art sketch pad.
-Click this link to visit website: https://guranshs.github.io/odin-etch-a-sketch/
+Click this link to visit website: [Here](https://guranshs.github.io/odin-etch-a-sketch/)
--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let penColor = "black";
 let penDown = true;
 
-function createBoard(size){
+function createBoard(size) {
     let board = document.querySelector('.board');
     let square = board.querySelectorAll("div");
-    square.forEach((div)=> div.remove());
-    board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
-    board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
-    
-    let amount = size*size;
-    for(let i=0; i<amount; i++){
+    square.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
         let square = document.createElement('div');
         square.addEventListener("mouseover", colorSquare);
         square.style.backgroundColor = "white";
@@ -19,46 +19,43 @@ function createBoard(size){
 
 createBoard(16);
 
-function changeSize(input){
-    if(input>=2 || input<=100){
+function changeSize(input) {
+    if (input >= 2 && input <= 100) {
         createBoard(input);
-    }
-    else{
-        console.log("Please input a valid number");
+    } else {
+        console.log("Please input a valid number between 2 and 100.");
     }
 }
 
-function colorSquare(){
-if(penDown){
-    if(penColor==="random"){
-        color1 = `hsl(${Math.random()*360}, 100%, 50%)`;
-        this.style.backgroundColor = color1;
-        document.querySelector(".randomButton").style.backgroundColor = color1;
+function colorSquare() {
+    if (penDown) {
+        if (penColor === "random") {
+            color1 = `hsl(${Math.random() * 360}, 100%, 50%)`;
+            this.style.backgroundColor = color1;
+            document.querySelector(".randomButton").style.backgroundColor = color1;
+        } else {
+            this.style.backgroundColor = penColor;
+        }
     }
-    else {
-        this.style.backgroundColor = penColor;
-    }
-}
 }
 
-function changePenColor(color){
+function changePenColor(color) {
     penColor = color;
 }
 
-function resetBoard(){
+function resetBoard() {
     let board = document.querySelector('.board');
     let square = board.querySelectorAll("div");
-    square.forEach((div)=> div.style.backgroundColor = "white");
+    square.forEach((div) => div.style.backgroundColor = "white");
 }
 
-document.querySelector("body").addEventListener("click", (e)=>{
-    if(e.target.tagName != "BUTTON"){
+document.querySelector("body").addEventListener("click", (e) => {
+    if (e.target.tagName != "BUTTON") {
         penDown = !penDown;
-        if(penDown){
-            document.querySelector(".mode").textContent = "Pen is down";
-        }
-        else{
-            document.querySelector(".mode").textContent = "Pen is up";
+        if (penDown) {
+            document.querySelector(".mode").textContent = "Pen is down. Click anywhere to lift the pen.";
+        } else {
+            document.querySelector(".mode").textContent = "Pen is up. Click anywhere to put the pen down.";
         }
     }
-})
+});
